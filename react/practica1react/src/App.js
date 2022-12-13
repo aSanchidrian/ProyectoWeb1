10f206b6e924667c3519033bfcd496927a1200a9
aSanchidrian <sanchidrian.alex@gmail.com>
2022-12-13 08:54:55 +0100

    dfs

--- react/practica1react/src/App.js
+++ react/practica1react/src/App.js
@@ -4,9 +4,13 @@ import axios from "axios";
 import { useState } from "react";
 
 import Player from "./Player";
+import Ganador from "./Ganador";
 
 function App() {
   const [name, setName] = useState([]);
+  const [data, setData] = useState([]);
+  const [num1, setNum1] = useState(0);
+  const [num2, setNum2] = useState(0);
 
   const sendGetRequest = async () => {
     try {
@@ -15,7 +19,22 @@ function App() {
       );
       setName(resp.data);
     } catch (err) {
-      // Handle Error Here
+      console.error(err);
+    }
+  };
+
+  function randomNumberInRange(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  const handleClick = () => {
+    setNum(randomNumberInRange(1, 10));
+  };
+
+  const getGanador = async () => {
+    try {
+      let n1 = {};
+    } catch (err) {
       console.error(err);
     }
   };
@@ -33,6 +52,36 @@ function App() {
         <tbody>
           {name.map((player) => (
             <Player player={player} />
+          ))}
+        </tbody>
+      </table>
+      <div className="d-flex justify-content-center">
+        <div className="p-2">
+          <button
+            type="button"
+            className="btn btn-primary"
+            onClick={handleClick}
+          >
+            Sortear
+          </button>
+        </div>
+        <div className="p-2">
+          <button type="button" className="btn btn-primary">
+            Reiniciar
+          </button>
+        </div>
+      </div>
+      <table class="table">
+        <thead>
+          <tr>
+            <th>Sorteo</th>
+            <th>Jugador 1</th>
+            <th>Jugador2</th>
+          </tr>
+        </thead>
+        <tbody>
+          {name.map((player) => (
+            <Ganador player={player} />
           ))}
         </tbody>
       </table>
---
+++ react/practica1react/src/Ganador.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ganador = ({ player }) => {
+  return (
+    <tr class="table-info">
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+    </tr>
+  );
+};
+
+export default ganador;
